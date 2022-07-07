715eaa765fd54609a53344fd1d1a82b6699faf01
Drozd0f <600dan600@gmail.com>
2022-07-07 09:59:55 +0300

    Update README.md

--- migrator/main.py
+++ migrator/main.py
@@ -8,9 +8,9 @@ from migrator.src.db import make_migration
 def main():
     ap = ArgumentParser(description='Module for migrate postgres database')
     ap.add_argument('-p', '--path', default=os.environ.get('MIGRATOR_MIGRATIONS_PATH'),
-                    required=True, help='Path to migrations')
+                    required=True, help='Path to migrations folder')
     ap.add_argument('-d', '--db-uri', default=os.environ.get('MIGRATOR_DB_URI'),
-                    required=True, help='URI for connect to database')
+                    required=True, help='Database connection URI')
     args = ap.parse_args()
     asyncio.run(make_migration(args.path, args.db_uri))
 
